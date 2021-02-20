75f719fc469800662c7b6b85ce6cccc640708705
Erdal256 <erdall.2693@gmail.com>
2021-02-20 20:12:00 +0300

    V9

--- DotNetMvc2/MoviesMvcBilgeAdam-master/036_MoviesMvcBilgeAdam/Contexts/MoviesContext.cs
+++ DotNetMvc2/MoviesMvcBilgeAdam-master/036_MoviesMvcBilgeAdam/Contexts/MoviesContext.cs
@@ -27,22 +27,22 @@ namespace _036_MoviesMvcBilgeAdam.Contexts
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            // Fluent API = Akıcı API
+            // Fluent API:
             modelBuilder.Entity<Review>()
-                 .HasRequired(review => review.Movie) //reguired(HesRequired8()) : mutlaka olmalı, optional(HesOptional()): olmasa da olur
-                 .WithMany(movie => movie.Reviews)
-                 .HasForeignKey(revies => revies.MovieId)
-                 .WillCascadeOnDelete(false); // veritabanındaki Review ile Movie arasındaki ilişkide delete rule no action olur
+                .HasRequired(review => review.Movie) // required (HasRequired()): mutlaka olmalı, optional (HasOptional()): olmasa da olur
+                .WithMany(movie => movie.Reviews)
+                .HasForeignKey(review => review.MovieId)
+                .WillCascadeOnDelete(false); // veritabanındaki Review ile Movie arasındaki ilişkide delete rule no action olur
             modelBuilder.Entity<MovieDirector>()
                 .HasRequired(movieDirector => movieDirector.Movie)
-                .WithMany(movive => movive.MovieDirectors)
+                .WithMany(movie => movie.MovieDirectors)
                 .HasForeignKey(movieDirector => movieDirector.MovieId)
                 .WillCascadeOnDelete(false);
             modelBuilder.Entity<MovieDirector>()
-              .HasRequired(movieDirector => movieDirector.Director)
-              .WithMany(director => director.MovieDirectors)
-              .HasForeignKey(movieDirector => movieDirector.DirectorId)
-              .WillCascadeOnDelete(false);
+                .HasRequired(movieDirector => movieDirector.Director)
+                .WithMany(director => director.MovieDirectors)
+                .HasForeignKey(movieDirector => movieDirector.DirectorId)
+                .WillCascadeOnDelete(false);
         }
     }
 }
